2d1afde7a8e906332187d833c5c4f70e7fc8a072
thoughtsociety <steve@thoughtsociety.org>
2018-10-03 10:26:44 -0400

    got the spacing on app.py right

--- dash/app.py
+++ dash/app.py
@@ -28,6 +28,8 @@ text_color = 'rgb(36,36,36)'
 bg_color = 'rgb(255,255,255)'
 grid_color = '#666666'
 black_text = '#000000'
+block_quote_text = 'rgb(64,64,64)'
+blue_text = 'rgb(51, 153, 255)'
 
 # Get Chryddyp's CSS for Dash from Codepen
 
@@ -71,58 +73,137 @@ for year in year_index_strings:
     options.append({'label':'{}'.format(year),'value':year_index[i]})
     i+=1
 
-#for tic in nsdq.index:
-#    options.append({'label':'{} {}'.format(tic,nsdq.loc[tic]['Name']), 'value':tic})
-
 
 app.layout=html.Div([   # top,rt,bot,lft
 
-    html.H3('Market Trends Over a Dozen Years',style={'color':'black','width':'97.33%',
-        'text-align':'center','background-color':bg_color,
-            'margin-bottom': '0px','padding':'10px 0px 0px 0px'}
-                                                         # 'padding':'10px 0px 100px 0px'})
-            ),
+
+# html.H3('Market Trends Over a Dozen Years',style={'color':'black',
+#     'text-align':'center','background-color':bg_color,
+#         'margin-bottom': '0px','padding':'10px 0px 0px 0px'}
+#
+#         ),
+
 
     # combi-plots with dropdown
 
-    html.Div([
-
+    html.Div([ # Upper-left div top
+    html.H3('Nasdaq and S&P 500 Comparison', style={'color': blue_text}),
+    dcc.Markdown('''***'''),
     dcc.Graph(id='djia_id'), # djia graph
+    dcc.Markdown('''***'''),
+    html.Div([ # for quoteblock
+    dcc.Markdown('''>Two indices in comparison: Looking at both the Nasdaq
+    and S&P-500, we can see that they both easily reflect the *crash of 2009*
+    right after the housing crisis starting taking effect.
+    ''')
 
-    dcc.Graph(id='ndxt_id'), # ndxt graph
-    ],style={'display':'inline-block'} #'width':'33.333%'
+        ], style={'height':'200px'}
+        ),
+    dcc.Markdown('''***'''),
+    ],style={'height':'200px','width':'40%',
+              'padding':'10px 40px 10px 40px','display':'inline-block','border-width':'1px'}
     ),
 
-    html.Div ([
-    dcc.Graph(id='djiaII_id'), # djia graph
+    html.Div([  # Upper-right div
+    html.H3('Nasdaq Tech Sector Closing Price', style={'color': blue_text}),
+    dcc.Markdown('''***'''),
+    dcc.Graph(id='ndxt_id'),  # djia graph
+    dcc.Markdown('''***'''),
+    html.Div([
+    dcc.Markdown('''>**The Nasdaq Tech Sector Index**
+    is a measure of the power of technology in the marketplace.
+    Since tech is the engine of the bull market right now, it pays to
+    keep a sharp eye on this sector now and into the future. **The Nasdaq Tech Sector Index**
+    is a measure of the power of technology in the marketplace.
+    Since tech is the engine of the bull market right now, it pays to
+    keep a sharp eye on this sector now and into the future.
+    
+   ''')
 
-    dcc.Graph(id='gspc_id'), # ndxt graph
-    ],style={'display':'inline-block'}
+        ], style={'height': '200px'}
+        ),
+    dcc.Markdown('''***'''),
+    ], style={'height':'200px','width': '40%',
+              'padding': '10px 40px 10px 40px', 'display': 'inline-block', 'border-width': '1px'}
     ),
 
-    html.Div ([
-    dcc.Graph(id='ixic2_id'), # djia graph
 
-    dcc.Graph(id='gspc2_id'), # ndxt graph
-    ],style={'display':'inline-block'}
-    ),
+    #dcc.Markdown('''***'''),
+    # ],style={'height':'200px','width':'40%',
+    #              'padding':'10px 40px 10px 40px','display':'inline-block'}
+    # )
+    #
+    # html.Div([
+    # dcc.Markdown('''***'''),
+    # dcc.Graph(id='ndxt_id'), # ndxt graph
+    # dcc.Markdown('''***'''),
+    #     html.Div([
+    #     dcc.Markdown('''>**Nasdaq Tech Sector Index** This is an index that
+    #     has paced major indices on its own. '''
+    #             )
+    #         ],style={'height':'200px'}
+    #         ),
+    # dcc.Markdown('''***'''),
+    # ],style={'height':'200px','display':'inline-block'} #'width':'33.333%'
+    # ),
+    #
+    # html.Div([
+    # dcc.Graph(id='djiaII_id'), # ndxt graph
+    # dcc.Markdown('''***'''),
+    #     html.Div([
+    #     dcc.Markdown('''>**Dow-Jones Industrial Averate ** This is an index that
+    #     has paced major indices on its own. '''
+    #     ),
+    #         ],style={'height':'200px'}
+    #     ),
+    # dcc.Markdown('''***'''),
+    # ],style={'height':'200px','display':'inline-block'} #'width':'33.333%'
+    # ),
+    #
+    # html.Div([
+    # dcc.Graph(id='gspc_id'), # ndxt graph
+    # dcc.Markdown('''***'''),
+    # dcc.Markdown('''>**Standard & Poor 500** This is an index that
+    # has paced major indices on its own. '''
+    # ),
+    # dcc.Markdown('''***'''),
+    # ],style={'height':'200px','display':'inline-block'} #'width':'33.333%'
+    # ),
+    #
+    # html.Div([
+    # dcc.Graph(id='ixic2_id'), # ixic graph
+    # dcc.Markdown('''***'''),
+    # dcc.Markdown('''>**Nasdaq Index** This is an index that
+    # has paced major indices on its own. '''
+    # ),
+    # dcc.Markdown('''***'''),
+    # ],style={'height':'200px','display':'inline-block'} #'width':'33.333%'
+    # ),
+    #
+    # html.Div([
+    # dcc.Graph(id='gspc2_id'), # s&p graph
+    # dcc.Markdown('''***'''),
+    # dcc.Markdown('''>**Standard & Poor 500** This is an index that
+    # has paced major indices on its own. '''
+    # ),
+    # dcc.Markdown('''***'''),
+    # ],style={'height':'200px','display':'inline-block'} #'width':'33.333%'
+    # ),
+    #
+html.Div(dcc.Slider(  # The years range slider
+            id='years-range-slider',
+            min=2006,
+            max=2018,
+            value=2018,
+            step=2,
+            marks= {i: '{}'.format(i) for i in year_index},
 
-    html.Div(dcc.Slider(  # The years range slider
-                id='years-range-slider',
-                min=2006,
-                max=2018,
-                value=2018,
-                step=2,
-                marks= {i: '{}'.format(i) for i in year_index},
+        ), style={'color':'red','margin-top': '0px','width': '97.33%',
+                  'padding':'30px 38px 30px 30px', # top,rt,bot,lft
+                  'background-color':bg_color}
+    )
 
-            ), style={'color':'red','margin-top': '0px','width': '97.33%',
-                      'padding':'30px 38px 30px 30px', # top,rt,bot,lft
-                      'background-color':bg_color}
-        )
-
-], style={'display': 'inline','font': {'color':text_color}, 'width': '97.33%',
-          'padding':'0px 0px 60px 0px','background-color':bg_color,
-          'margin-bottom': '0px'}
+]
 
 )
 
@@ -133,12 +214,12 @@ app.layout=html.Div([   # top,rt,bot,lft
 
 # button callbacks to navigate between the three apps
 
-#@app.callback(
+# @app.callback(
 #    Input('dash1_button',pressed)
-#def select_dash1_graph(pressed):
+# def select_dash1_graph(pressed):
 #    if pressed:
 #        html
-#)
+# )
 
 @app.callback( # Stock # 1 - DJIA
     Output('djia_id', 'figure'),
@@ -151,15 +232,15 @@ def update_stock_graph(value):
     # cutoff = (value - first_year) * 12
 
     '''
-    To use the dropdown, produce a slice range that grabs just the rows 
+    To use the dropdown, produce a slice range that grabs just the rows
     from the selected year and copies it into the f_djia df
     each date has 12 rows of data - 1 for each month
     To set a lower and upper mask to slice the df, it is the first row of the chosen date +12
-    
+
     value = integer year.  years go from 2006 - 2018 or 6-18.  This is essentially a range of 12 years.
     For 12 months.  So the index range is 0-143.  If the selected year is 2010
-    
-    
+
+
     '''
 
     #year_val = (int(value) - 2005) - 1
@@ -199,7 +280,6 @@ def update_stock_graph(value):
     #
     # }
     # }
-
     fig = {
         'data': [
             go.Scatter(
@@ -211,7 +291,6 @@ def update_stock_graph(value):
                     'color': 'rgb(153, 0, 255)'
                 },
                 name='Nasdaq',
-
             ),
             go.Scatter(
                 x=f_ixic['Date'],
@@ -222,22 +301,15 @@ def update_stock_graph(value):
                     'color': 'rgb(255, 0, 153)'
                 },
                 name='S&P-500',
-
             ),
-
         ],
-        'layout':{ 'title': 'Nasdaq and S&P 500 Comparison',
-                   'paper_bgcolor': bg_color, 'plot_bgcolor': bg_color,
+        'layout':{ 'paper_bgcolor': bg_color, 'plot_bgcolor': bg_color,
                    'font': {'color': text_color},
                    'xaxis': {'title':'Years','gridcolor': grid_color, 'range': [cutoff - first_year], 'step': 1},
                    'yaxis': {'title':'Closing Price','gridcolor': grid_color},
                    'auto_size': False,
                    'width': 433,
                    'height': 400
-
-
-
-
                 }
         }
     return fig
@@ -256,8 +328,7 @@ def update_stock_graph(value):
 
     fig = {
         'data': traces,
-        'layout': {'title':'Nasdaq Tech Sector Closing Price',
-                   'paper_bgcolor':bg_color,'plot_bgcolor':bg_color,'font': {'color':text_color},
+        'layout': {'paper_bgcolor':bg_color,'plot_bgcolor':bg_color,'font': {'color':text_color},
                    'xaxis': {'gridcolor': grid_color},
                    'yaxis': {'gridcolor': grid_color} ,
                    'auto_size': False,
@@ -266,102 +337,102 @@ def update_stock_graph(value):
                    }
     }
     return fig
-
-@app.callback( # Stock #3 IXIC
-    Output('djiaII_id', 'figure'),
-    [Input('years-range-slider', 'value')])
-def update_stock_graph(value):
-
-    traces = []
-    cutoff = (value - first_year) * 12
-    f_djia = djia[0:cutoff]
-
-    traces.append({'x': f_djia['Date'], 'y': f_djia['Close'], 'name': 'djia'}),
-
-    fig = {
-        'data': traces,
-        'layout': {'title':'Djia Closing Price',
-                   'paper_bgcolor':bg_color,'plot_bgcolor':bg_color,'font': {'color':text_color},
-                   'xaxis': {'gridcolor': grid_color},
-                   'yaxis': {'gridcolor': grid_color} ,
-                   'auto_size': False,
-                   'width': 433,
-                   'height': 400
-                   }
-    }
-    return fig
-
-@app.callback( # Stock #4 GSPC
-    Output('gspc_id', 'figure'),
-    [Input('years-range-slider', 'value')])
-def update_stock_graph(value):
-
-    traces = []
-    cutoff = (value - first_year) * 12
-    f_gspc = gspc[0:cutoff]
-
-    traces.append({'x': f_gspc['Date'], 'y': f_gspc['Close'], 'name': 'gspc'})
-    fig = {
-        'data': traces,
-        'layout': {'title':'S&P 500 Closing Price',
-                   'paper_bgcolor':bg_color,'plot_bgcolor':bg_color,'font': {'color':text_color},
-                   'xaxis': {'gridcolor': grid_color},
-                   'yaxis': {'gridcolor': grid_color} ,
-                   'auto_size': False,
-                   'width':433,
-                   'height':400
-                   }
-    }
-    return fig
-
-@app.callback( # Stock #3 IXIC
-    Output('ixic2_id', 'figure'),
-    [Input('years-range-slider', 'value')])
-def update_stock_graph(value):
-
-    traces = []
-    cutoff = (value - first_year) * 12
-    f_ixic = ixic[0:cutoff]
-
-    traces.append({'x': f_ixic['Date'], 'y': f_ixic['Close'], 'name': 'ixic2'}),
-
-    fig = {
-        'data': traces,
-        'layout': {'title':'Nasdaq-100 Closing Price',
-                   'paper_bgcolor':bg_color,'plot_bgcolor':bg_color,'font': {'color':text_color},
-                   'xaxis': {'gridcolor': grid_color},
-                   'yaxis': {'gridcolor': grid_color} ,
-                   'auto_size': False,
-                   'width':433,
-                   'height':400
-                   }
-    }
-    return fig
-
-@app.callback( # Stock #4 GSPC
-    Output('gspc2_id', 'figure'),
-    [Input('years-range-slider', 'value')])
-def update_stock_graph(value):
-
-    traces = []
-    cutoff = (value - first_year) * 12
-    f_gspc = gspc[0:cutoff]
-
-    traces.append({'x': f_gspc['Date'], 'y': f_gspc['Close'], 'name': 'gspc2'})
-    fig = {
-        'data': traces,
-        'layout': {'title':'S&P 500 Closing Price',
-                   'paper_bgcolor':bg_color,'plot_bgcolor':bg_color,'font': {'color':text_color},
-                   'xaxis': {'gridcolor': grid_color},
-                   'yaxis': {'gridcolor': grid_color} ,
-                   'auto_size': False,
-                   'width':433,
-                   'height':400
-                   }
-    }
-    return fig
+#
+# @app.callback( # Stock #3 IXIC
+#     Output('djiaII_id', 'figure'),
+#     [Input('years-range-slider', 'value')])
+# def update_stock_graph(value):
+#
+#     traces = []
+#     cutoff = (value - first_year) * 12
+#     f_djia = djia[0:cutoff]
+#
+#     traces.append({'x': f_djia['Date'], 'y': f_djia['Close'], 'name': 'djia'}),
+#
+#     fig = {
+#         'data': traces,
+#         'layout': {'title':'Djia Closing Price',
+#                    'paper_bgcolor':bg_color,'plot_bgcolor':bg_color,'font': {'color':text_color},
+#                    'xaxis': {'gridcolor': grid_color},
+#                    'yaxis': {'gridcolor': grid_color} ,
+#                    'auto_size': False,
+#                    'width': 433,
+#                    'height': 400
+#                    }
+#     }
+#     return fig
+#
+# @app.callback( # Stock #4 GSPC
+#     Output('gspc_id', 'figure'),
+#     [Input('years-range-slider', 'value')])
+# def update_stock_graph(value):
+#
+#     traces = []
+#     cutoff = (value - first_year) * 12
+#     f_gspc = gspc[0:cutoff]
+#
+#     traces.append({'x': f_gspc['Date'], 'y': f_gspc['Close'], 'name': 'gspc'})
+#     fig = {
+#         'data': traces,
+#         'layout': {'title':'S&P 500 Closing Price',
+#                    'paper_bgcolor':bg_color,'plot_bgcolor':bg_color,'font': {'color':text_color},
+#                    'xaxis': {'gridcolor': grid_color},
+#                    'yaxis': {'gridcolor': grid_color} ,
+#                    'auto_size': False,
+#                    'width':433,
+#                    'height':400
+#                    }
+#     }
+#     return fig
+#
+# @app.callback( # Stock #3 IXIC
+#     Output('ixic2_id', 'figure'),
+#     [Input('years-range-slider', 'value')])
+# def update_stock_graph(value):
+#
+#     traces = []
+#     cutoff = (value - first_year) * 12
+#     f_ixic = ixic[0:cutoff]
+#
+#     traces.append({'x': f_ixic['Date'], 'y': f_ixic['Close'], 'name': 'ixic2'}),
+#
+#     fig = {
+#         'data': traces,
+#         'layout': {'title':'Nasdaq-100 Closing Price',
+#                    'paper_bgcolor':bg_color,'plot_bgcolor':bg_color,'font': {'color':text_color},
+#                    'xaxis': {'gridcolor': grid_color},
+#                    'yaxis': {'gridcolor': grid_color} ,
+#                    'auto_size': False,
+#                    'width':433,
+#                    'height':400
+#                    }
+#     }
+#     return fig
+#
+# @app.callback( # Stock #4 GSPC
+#     Output('gspc2_id', 'figure'),
+#     [Input('years-range-slider', 'value')])
+# def update_stock_graph(value):
+#
+#     traces = []
+#     cutoff = (value - first_year) * 12
+#     f_gspc = gspc[0:cutoff]
+#
+#     traces.append({'x': f_gspc['Date'], 'y': f_gspc['Close'], 'name': 'gspc2'})
+#     fig = {
+#         'data': traces,
+#         'layout': {'title':'S&P 500 Closing Price',
+#                    'paper_bgcolor':bg_color,'plot_bgcolor':bg_color,'font': {'color':text_color},
+#                    'xaxis': {'gridcolor': grid_color},
+#                    'yaxis': {'gridcolor': grid_color} ,
+#                    'auto_size': False,
+#                    'width':433,
+#                    'height':400
+#                    }
+#     }
+#     return fig
 
 
 if __name__ == '__main__':
     #app.run_server(ssl_context='adhoc')
-    app.run_server()
+    app.run_server(debug=True)
